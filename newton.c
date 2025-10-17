dbf7250cfe6672d837f5bb8f5dc55aa4d2f12e75
peogradin <peo.gradin@live.se>
2025-10-17 13:58:44 +0200

    Got synchronization working

--- newton.c
+++ newton.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 #include <math.h>
 #include <threads.h>
+#include <time.h>
 
 float min_val = -2;
 float max_val = 2;
@@ -35,8 +36,9 @@ typedef struct {
 
 typedef struct {
   const float *v;
+  int **root_idx_res;
   int sz;
-  int n_threads;
+  int n_thrds;
   mtx_t *mtx;
   cnd_t *cnd;
   int_padded *status;
@@ -51,6 +53,7 @@ int main(
   char *argv[]
 )
 {
+
   parse_args(argc, argv);
 
   for ( int i = 0; i < d; ++i ) {
@@ -74,11 +77,53 @@ int main(
   cnd_init(&cnd);
 
   int_padded status[n_thrds];
+  for ( int tx = 0; tx < n_thrds; ++tx ) {
+    thrds_info[tx].v = (const float*) v;
+    thrds_info[tx].root_idx_res = root_idx_res;
+    thrds_info[tx].ib = tx;
+    thrds_info[tx].istep = n_thrds;
+    thrds_info[tx].sz = sz;
+    thrds_info[tx].tx = tx;
+    thrds_info[tx].mtx = &mtx;
+    thrds_info[tx].cnd = &cnd;
+    thrds_info[tx].status = status;
+    status[tx].val = -1;
+
+    int r = thrd_create(thrds + tx, main_thrd, (void*) (thrds_info + tx));
+    if ( r != thrd_success ) {
+      fprintf(stderr, "Failed to create thread\n");
+      exit(1);
+    }
+    thrd_detach(thrds[tx]);
+  }
+  {
+    thrd_info_write.v = (const float*) v;
+    thrd_info_write.root_idx_res = (int**) root_idx_res;
+    thrd_info_write.sz = sz;
+    thrd_info_write.n_thrds = n_thrds;
+    thrd_info_write.mtx = &mtx;
+    thrd_info_write.cnd = &cnd;
+    thrd_info_write.status = status;
+
+    int r = thrd_create(&write_thrd, main_thrd_write, (void*)(&thrd_info_write));
+    if ( r != thrd_success ) {
+      fprintf(stderr, "Failed to create thread\n");
+      exit(1);
+    }
+  }
+
+  {
+    int r;
+    thrd_join(write_thrd, &r);
+  }
 
   free(v);
   free(root_idx_res);
   free(roots_r);
   free(roots_c);
+
+  mtx_destroy(&mtx);
+  cnd_destroy(&cnd);
 
   return 0;
 }
@@ -104,11 +149,17 @@ int main_thrd(
 
     for ( int jx = 0; jx < sz; ++jx ) {
       // Insert computations here or something
-      root_idx_row[jx] = ix;
+      // For now, we do a simple implementation to check functionality of threading/mtx/cnd
+      root_idx_row[jx] = ix*jx;
     }
 
+    mtx_lock(mtx);
+    root_idx_res[ix] = root_idx_row;
+    status[tx].val = ix + istep;
+    mtx_unlock(mtx);
+    cnd_signal(cnd);
 
-
+    thrd_sleep(&(struct timespec){.tv_sec = 0, .tv_nsec = 1000}, NULL);
   }
 
   return 0;
@@ -118,6 +169,43 @@ int main_thrd_write(
   void *args
 )
 {
+  const thrd_info_write_t *thrd_info = (thrd_info_write_t*) args;
+  const float *v = thrd_info->v;
+  int **root_idx_res = thrd_info->root_idx_res;
+  const int sz = thrd_info->sz;
+  const int n_thrds = thrd_info->n_thrds;
+  mtx_t *mtx = thrd_info->mtx;
+  cnd_t *cnd = thrd_info->cnd;
+  int_padded *status = thrd_info->status;
+
+  for ( int ix = 0, ibnd; ix < sz; ) {
+    
+    for ( mtx_lock(mtx); ; ) {
+      ibnd = sz;
+      for ( int tx = 0; tx < n_thrds; ++tx ) {
+        if ( ibnd > status[tx].val ) ibnd = status[tx].val;
+      }
+
+      if ( ibnd <= ix ) {
+        cnd_wait(cnd, mtx);
+      }
+      else {
+        mtx_unlock(mtx);
+        break;
+      }
+    }
+
+    printf("\n\nWriting until %i\n", ibnd);
+    for ( ; ix < ibnd; ++ix ) {
+      // Insert write to file here
+      // For now, we do a dummy print to check threading/mtx/cnd functionality
+      int res = root_idx_res[ix][ix];
+      printf("Index: %i - value: %i\n", ix, res);
+      thrd_sleep(&(struct timespec){.tv_sec=0, .tv_nsec=1000}, NULL);
+      free(root_idx_res[ix]);
+    }
+  }
+
   return 0;
 }
 
@@ -158,5 +246,3 @@ void parse_args(
     roots_c[i] = (float)sin(angle);
   }
 }
-
-
